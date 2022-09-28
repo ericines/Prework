6645d4b3b56df99383c6c87fa66ca9ac29edba93
ericines <ericines@outlook.com>
2022-09-28 18:10:31 -0400

    Create Exercise-2.js

---
+++ Exercise-2.js
@@ -0,0 +1,23 @@
+//Display 'Hello WOrld!' to the console.
+console.log('Hello Worldd!');
+
+console.log("Hello World!"); //Also displays 'Hello World!' too
+
+console.log('Hello World!');
+
+//console.log('This WON'T be displayed.');
+
+console.log("This WILL be displayed.");
+
+/*This is a multi-line comment.
+There isn't a lot to say about this simple example,
+but  you can imagine how it would be helpful for longer descriptions
+of more complex code*/
+
+console.log("This will be displayed.");
+
+/*
+console.log("But this will not be ddisplayed");
+console.log("And neither will this");
+console.log("Or this");
+*/
