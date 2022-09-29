c9faf68b5dcea7329600dd0e82b1b8bf016c813d
ericines <ericines@outlook.com>
2022-09-28 21:33:27 -0400

    Update Exercise-3.js

--- Exercise-3.js
+++ Exercise-3.js
@@ -4,6 +4,6 @@ console.log("100");
 console.log("My name is: ");
 console.log(); //change this line
 console.log("My age is:  ");
-console.log(); //change this line, which type should you use?
+console.log(27); //change this line, which type should you use?
 console.log("My zip code is: ");
-console.log(); //change this line, which type should you use?
+console.log("33778"); //change this line, which type should you use?
