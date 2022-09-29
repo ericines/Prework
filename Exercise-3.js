4f8dea303123e2ef299790e727170afdf46c61ca
ericines <ericines@outlook.com>
2022-09-28 21:37:44 -0400

    Update Exercise-3.js

--- Exercise-3.js
+++ Exercise-3.js
@@ -2,8 +2,8 @@ console.log("This is a string, it has quotes.");
 console.log(100);
 console.log("100");
 console.log("My name is: ");
-console.log(); //change this line
+console.log("Eric");
 console.log("My age is:  ");
-console.log(27); //change this line, which type should you use?
+console.log(27);
 console.log("My zip code is: ");
-console.log("33778"); //change this line, which type should you use?
+console.log("33778");
