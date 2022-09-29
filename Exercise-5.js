0f143cbea0198a02926c07502971a03841333fd9
ericines <ericines@outlook.com>
2022-09-28 21:55:39 -0400

    Create Exercise-5.js

---
+++ Exercise-5.js
@@ -0,0 +1,10 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11);
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+console.log(10 == 10); // ==, true
+console.log(10 > 11); // >, false
+console.log(10 <= 9); // <=, false
+console.log(10 != 11); // !=, true
